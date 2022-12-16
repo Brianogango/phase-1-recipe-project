a7d303a73ed2bb67a982b8ad36de207f64c9bc12
brian <brian.ogango@student.moringaschool.com>
2022-12-16 18:59:11 +0300

    updated

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function recipes(recipes) {
   
     `;
     const card =document.getElementById("card")
-    card.addEventListener("click", flipCard);
+    card.addEventListener("click", flipCard());
     function flipCard(){
     card.classList.toggle("flipCard");
 }
