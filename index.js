7a3993ffebac9bad48d1d0d91d6ce8ea640e487d
brian <brian.ogango@student.moringaschool.com>
2022-12-16 10:27:13 +0300

    added the cardflip event listener

--- index.js
+++ index.js
@@ -37,4 +37,10 @@ function recipes(recipes) {
         </div>
   
     `;
+    const card =document.getElementById("card")
+    card.addEventListener("click", flipCard);
+    function flipCard(){
+    card.classList.toggle("flipCard");
+}
+
 }
