38857ef11305569187ff66f553e25000b62c772d
brian <brian.ogango@student.moringaschool.com>
2022-12-16 10:26:16 +0300

    each recipe rendering

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ function searchRecipes(query) {
         recipes([]);
         });
  }
+ //----Rendering each recipe on the web
+function recipes(recipes) {
+    console.log(recipes)
+    document.querySelector('#display_section').innerHTML+=`
+
+    <div class="card" id="card">
+            <div class="front">
+                <div class="image">
+                <img src="${recipes.hits[0].recipe.image}" alt="try">
+                </div>
+                <div class="name"><h2>${recipes.hits[0].recipe.label}</h2> </div>
+                <div class="recipe_btn" id="open" ><a href = "#" class = "submit">Get Recipe</a></div>
+            </div>
+            <div class="back">
+                <h2>${recipes.hits[0].recipe.label}</h2>
+                <div class="back_image">
+                <img src="${recipes.hits[0].recipe.image}" alt="">
+                </div>
+                <div class="ingredients">
+                <h3>Ingredients</h3>
+                <p>${recipes.hits[0].recipe.ingredientLines}</p>
+                </div>
+            </div>
+        </div>
+  
+    `;
+}
